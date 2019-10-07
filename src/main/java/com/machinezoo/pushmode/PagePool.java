42340390ed19588b598e0e714655a6312192b124
Robert Vazan <robvcode@gmail.com>
2019-10-07 18:54:13 +0000

    Client communication servlets

---
+++ src/main/java/com/machinezoo/pushmode/PagePool.java
@@ -0,0 +1,67 @@
+// Part of PushMode: https://pushmode.machinezoo.com
+package com.machinezoo.pushmode;
+
+import java.time.*;
+import java.util.*;
+import org.slf4j.*;
+import com.machinezoo.noexception.*;
+import io.micrometer.core.instrument.*;
+
+public class PagePool {
+	private static final PagePool instance = new PagePool();
+	public static PagePool instance() {
+		return instance;
+	}
+	private static final Logger logger = LoggerFactory.getLogger(PagePool.class);
+	private Map<String, PoolEntry> pool = Metrics.gauge("pushmode.pool.size", new HashMap<>(), m -> instance.size());
+	private Duration neverExpireBefore = Duration.ofSeconds(70);
+	private Duration alwaysExpireAfter = Duration.ofMinutes(10);
+	private int capacity = 2000;
+	private double purgeRatio = 0.8;
+	private int purgeThreshold = capacity;
+	private PagePool() {
+	}
+	public synchronized void add(PushPage page) {
+		PoolEntry entry = new PoolEntry(page);
+		pool.put(page.pageId(), entry);
+		Exceptions.log(logger).run(this::purge);
+	}
+	public synchronized PushPage lookup(String id) {
+		PoolEntry entry = pool.get(id);
+		if (entry != null) {
+			entry.access();
+			return entry.page;
+		}
+		return null;
+	}
+	private void purge() {
+		if (pool.size() >= purgeThreshold) {
+			List<PoolEntry> sorted = new ArrayList<>(pool.values());
+			sorted.sort(Comparator.comparing(e -> e.lastAccessTime));
+			Instant now = Instant.now();
+			Instant hardLimit = now.minus(alwaysExpireAfter);
+			Instant softLimit = now.minus(neverExpireBefore);
+			int targetSize = (int)(purgeRatio * capacity);
+			for (PoolEntry entry : sorted) {
+				if (entry.lastAccessTime.isBefore(hardLimit) || pool.size() > targetSize && entry.lastAccessTime.isBefore(softLimit))
+					pool.remove(entry.page.pageId());
+				else
+					break;
+			}
+			purgeThreshold = Math.max((int)(pool.size() / purgeRatio), capacity);
+		}
+	}
+	private synchronized int size() {
+		return pool.size();
+	}
+	private class PoolEntry {
+		final PushPage page;
+		PoolEntry(PushPage page) {
+			this.page = page;
+		}
+		Instant lastAccessTime = Instant.now();
+		void access() {
+			lastAccessTime = Instant.now();
+		}
+	}
+}
---
+++ src/main/java/com/machinezoo/pushmode/PollServlet.java
@@ -0,0 +1,83 @@
+// Part of PushMode: https://pushmode.machinezoo.com
+package com.machinezoo.pushmode;
+
+import java.nio.*;
+import java.nio.charset.*;
+import java.time.*;
+import javax.servlet.http.*;
+import org.apache.http.*;
+import org.apache.http.client.utils.*;
+import com.fasterxml.jackson.databind.*;
+import com.machinezoo.hookless.*;
+import com.machinezoo.hookless.servlets.*;
+import com.machinezoo.hookless.time.*;
+import com.machinezoo.noexception.*;
+import io.micrometer.core.instrument.*;
+
+@SuppressWarnings("serial") public class PollServlet extends ReactiveServlet {
+	private static final Counter reloadCounter = Metrics.counter("pushmode.poll.reloads");
+	private static final Counter exceptionCounter = Metrics.counter("pushmode.poll.exceptions");
+	private static final Counter requestCounter = Metrics.counter("pushmode.poll.requests");
+	private static final Counter patchCounter = Metrics.counter("pushmode.poll.patches");
+	private static final Counter trafficCounter = Metrics.counter("pushmode.poll.traffic");
+	private static final LongTaskTimer activeTimer = LongTaskTimer.builder("pushmode.poll.active").register(Metrics.globalRegistry);
+	private static final ObjectMapper mapper = new ObjectMapper();
+	@Override public ReactiveServletResponse doPost(ReactiveServletRequest request) {
+		Instant start = CurrentReactiveScope.pin("start", Instant::now);
+		LongTaskTimer.Sample sample = CurrentReactiveScope.pin("sample", activeTimer::start);
+		try {
+			if (ReactiveInstant.now().isAfter(start.plusSeconds(25)))
+				return new ReactiveServletResponse().status(HttpServletResponse.SC_NO_CONTENT);
+			String buster = Exceptions.sneak().get(() -> new URIBuilder(request.url())).getQueryParams().stream()
+				.filter(p -> p.getName().equals("v"))
+				.findFirst()
+				.map(NameValuePair::getValue)
+				.orElse(null);
+			if (!PushScriptServlet.buster().equals(buster)) {
+				reloadCounter.increment();
+				return new ReactiveServletResponse().status(HttpServletResponse.SC_RESET_CONTENT);
+			}
+			JsonNode json = Exceptions.sneak().get(() -> mapper.readTree(request.data()));
+			/*
+			 * Various bots will try to query this endpoint without supplying proper request body.
+			 * We will detect missing or bogus body and return appropriate HTTP status in order to avoid
+			 * unnecessary exceptions in logs later when we try to use values from the JSON body.
+			 */
+			if (json == null)
+				return new ReactiveServletResponse().status(HttpServletResponse.SC_BAD_REQUEST);
+			String id = json.get("s").asText();
+			long sequence = json.get("o").asLong();
+			PushPage page = CurrentReactiveScope.pin("page", () -> {
+				requestCounter.increment();
+				return PagePool.instance().lookup(id);
+			});
+			if (page == null) {
+				reloadCounter.increment();
+				return new ReactiveServletResponse().status(HttpServletResponse.SC_RESET_CONTENT);
+			}
+			PageFrame frame;
+			try {
+				frame = page.frame(sequence);
+			} catch (Throwable ex) {
+				exceptionCounter.increment();
+				return new ReactiveServletResponse().status(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			}
+			if (CurrentReactiveScope.blocked())
+				return new ReactiveServletResponse();
+			if (frame == null) {
+				reloadCounter.increment();
+				return new ReactiveServletResponse().status(HttpServletResponse.SC_RESET_CONTENT);
+			}
+			patchCounter.increment();
+			byte[] serialized = frame.jsonPatch().toString().getBytes(StandardCharsets.UTF_8);
+			trafficCounter.increment(serialized.length);
+			ReactiveServletResponse response = new ReactiveServletResponse();
+			response.headers().put("Content-Type", "application/json; charset=utf-8");
+			response.data(ByteBuffer.wrap(serialized));
+			return response;
+		} finally {
+			if (!CurrentReactiveScope.blocked())
+				sample.stop();
+		}
+	}
+}
