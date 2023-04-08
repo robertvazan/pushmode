13933fa43db214440eaec820fb712e36ef7b66f1
Robert Važan <robert.vazan@tutanota.com>
2023-04-08 12:52:26 +0000

    Updated project configuration: build PRs, updated CONTRIBUTING.md, upgraded stagean and jackson, added UA banner

--- scripts/configure.py
+++ scripts/configure.py
@@ -1,42 +1,37 @@
 # This script generates and updates project configuration files.
 
-# We are assuming that project-config is available in sibling directory.
-# Checkout from https://github.com/robertvazan/project-config
-import os.path
-import sys
-sys.path.append(os.path.normpath(os.path.join(__file__, '../../../project-config/src')))
+# Run this script with rvscaffold in PYTHONPATH
+import rvscaffold as scaffold
 
-from java import *
+class Project(scaffold.Java):
+    def script_path_text(self): return __file__
+    def repository_name(self): return 'pushmode'
+    def pretty_name(self): return 'PushMode'
+    def pom_description(self): return "Server-side Java library that streams web app's HTML output down to the browser while user's actions are streamed back to the server."
+    def inception_year(self): return 2015
+    def complete_javadoc(self): return False
+    def stagean_annotations(self): return True
+    def project_status(self): return f'''\
+        {self.experimental_status()}
+        Read-only streaming of HTML to the client is fairly stable. Streaming of user input to the server is early alpha.
+        Browser APIs (e.g., history manipulation) aren't implemented at all.
+    '''
+    
+    def dependencies(self):
+        yield from super().dependencies()
+        yield self.use_hookless()
+        yield self.use('com.machinezoo.hookless:hookless-servlets:0.5.2')
+        # Used for JSON messages exchanged with client-side JS.
+        yield self.use_jackson()
+        yield self.use_commons_io()
+        # Used for URL parsing only.
+        yield self.use('org.apache.httpcomponents:httpclient:4.5.13')
+        yield self.use_junit()
+        yield self.use_hamcrest()
+        yield self.use_slf4j_test()
+    
+    def javadoc_links(self):
+        yield 'https://stagean.machinezoo.com/javadoc/'
+        yield 'https://hookless.machinezoo.com/javadocs/servlets/'
 
-project_script_path = __file__
-repository_name = lambda: 'pushmode'
-pretty_name = lambda: 'PushMode'
-pom_description = lambda: "Server-side Java library that streams web app's HTML output down to the browser while user's actions are streamed back to the server."
-inception_year = lambda: 2015
-jdk_version = lambda: 11
-complete_javadoc = lambda: False
-stagean_annotations = lambda: True
-project_status = lambda: f'''\
-    {experimental_status()}
-    Read-only streaming of HTML to the client is fairly stable. Streaming of user input to the server is early alpha.
-    Browser APIs (e.g., history manipulation) aren't implemented at all.
-'''
-
-def dependencies():
-    use_hookless()
-    use('com.machinezoo.hookless:hookless-servlets:0.5.2')
-    # Used for JSON messages exchanged with client-side JS.
-    use_jackson()
-    use_commons_io()
-    # Used for URL parsing only.
-    use('org.apache.httpcomponents:httpclient:4.5.13')
-    use_junit()
-    use_hamcrest()
-    use_slf4j_test()
-
-javadoc_links = lambda: [
-    'https://stagean.machinezoo.com/javadoc/',
-    'https://hookless.machinezoo.com/javadocs/servlets/'
-]
-
-generate(globals())
+Project().generate()
